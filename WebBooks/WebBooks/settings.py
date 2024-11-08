b1e1a49e503c274cbb6d66f830298a78ab4545cc
Kustikphphph <puppy.maria@yandex.ru>
2024-11-09 01:33:19 +0300

    09.11.2024 л18 л19  Создание структуры сайта при помощи Django.Администрирование сайта при помощи Django Admin.Административная панель Django Admin.Работа с данными в Django Admin. Шевелева

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'WebBooks.urls'
+
 
 TEMPLATES = [
     {
@@ -122,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
